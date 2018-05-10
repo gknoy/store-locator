11e2da0388d53236a844528c66b900d7e2c8ce4c
Gabriel Knoy <gabriel.knoy@gmail.com>
2018-05-09 22:02:19 -0700

    Add tests of great_circle_distance

--- tests/test_find_store.py
+++ tests/test_find_store.py
@@ -4,9 +4,10 @@
 
 import unittest
 from decimal import Decimal
+from math import radians
 from find_store import (
     # find_store,
-    # great_circle_distance,
+    great_circle_distance,
     Store,
     # NYI: testing of loading API keys, or geocoding
 )
@@ -75,6 +76,72 @@ class StoreTest(unittest.TestCase):
             '    Longitude: -96.123\n'
         )
         self.assertEqual(s, expected)
+
+
+class GreatCircleDistanceTest(unittest.TestCase):
+
+    def _to_radians(self, deg=0, min=0, sec=0):
+        """Example locations are given in (deg, min, sec), but great_circle_distance needs radians"""
+        return radians(deg + (min / 60) + (sec / 3600))
+
+    def test_great_circle_distance(self):
+        """
+        Verify that we calculate great circle distance between arbitrary (latitude, longitude) locations.
+        Test data generated with [https://www.greatcirclemapper.net]
+        """
+        _r = self._to_radians
+        routes = [
+            {
+                'a': {
+                    'name': 'LHR',
+                    'lat': _r(51, 28, 14),
+                    'long': _r(0, 27, 42),
+                },
+                'b': {
+                    'name': 'JFK',
+                    'lat': _r(40, 38, 23),
+                    'long': _r(73, 46, 44),
+                },
+                'km': 5555,
+                'mi': 3551.7,
+            },
+            {
+                'a': {
+                    'name': 'SFO',
+                    'lat': _r(37, 37, 8),
+                    'long': _r(122, 22, 30),
+                },
+                'b': {
+                    'name': 'PDX',
+                    'lat': _r(45, 35, 19),
+                    'long': _r(122, 35, 52),
+                },
+                'km': 885,
+                'mi': 549.9,
+            },
+            {
+                'a': {
+                    'name': 'STS',
+                    'lat': _r(38, 30, 32),
+                    'long': _r(122, 48, 46),
+                },
+                'b': {
+                    'name': 'SFO',
+                    'lat': _r(37, 37, 8),
+                    'long': _r(122, 22, 30),
+                },
+                'km': 106,
+                'mi': 65.865,
+            },
+        ]
+        margin = 0.03
+        for route in routes:
+            a = (route['a']['lat'], route['a']['long'])
+            b = (route['b']['lat'], route['b']['long'])
+            km_delta = Decimal(margin * route['km'])
+            mi_delta = Decimal(margin * route['mi'])
+            self.assertAlmostEqual(Decimal(route['km']), great_circle_distance(a, b, km=True), delta=km_delta)
+            self.assertAlmostEqual(Decimal(route['mi']), great_circle_distance(a, b, km=False), delta=mi_delta)
 
 
 if __name__ == '__main__':
